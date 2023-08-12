8275939cf68e13605824e6cc9231030f8fb8f06d
gastoncc <131544505+gastoncc@users.noreply.github.com>
2023-08-12 19:17:37 -0300

    Tercera preentrega

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 
-// Objeto JSON para almacenar información del producto
-const producto = {
-  nombre: "ESMALTADO SEMIPERMANENTE",
-  descripcion: "El semipermanente es un sistema de duración corta...",
-  precio: 10.99,
-};
+function terceraPreentrega() {
+  const producto = {
+    nombre: "ESMALTADO SEMIPERMANENTE",
+    descripcion: "El semipermanente es un sistema de duración corta...",
+    precio: 10.99,
+  };
 
-// Almacenar el objeto JSON en el Local Storage
-localStorage.setItem("producto", JSON.stringify(producto));
+  localStorage.setItem("producto", JSON.stringify(producto));
 
-// Modificar el DOM para mostrar el precio del producto
-const precioParrafo = document.createElement("p");
-precioParrafo.textContent = `Precio: $${producto.precio}`;
-document.querySelector(".card-body").appendChild(precioParrafo);
+  const precioParrafo = document.createElement("p");
+  precioParrafo.textContent = `Precio: $${producto.precio}`;
+  document.querySelector(".card-body").appendChild(precioParrafo);
 
-// Detección de eventos de usuario
-document.getElementById("pedidoBtn").addEventListener("click", () => {
-  alert("¡Pedido realizado!");
-});
+  document.getElementById("pedidoBtn").addEventListener("click", () => {
+    alert("¡Pedido realizado!");
+  });
 
-// Bloque condicional para cambiar el color del precio según el precio
-if (producto.precio > 15) {
-  precioParrafo.style.color = "red";
-} else {
-  precioParrafo.style.color = "green";
+  if (producto.precio > 15) {
+    precioParrafo.style.color = "red";
+  } else {
+    precioParrafo.style.color = "green";
+  }
+
+  let stockDisponible = true;
+  if (stockDisponible) {
+    alert("El producto está disponible en stock.");
+  } else {
+    alert("El producto no está disponible en stock.");
+  }
+
+  const productosArray = [
+    { nombre: "Producto A", precio: 8.99 },
+    { nombre: "Producto B", precio: 12.49 },
+    { nombre: "Producto C", precio: 15.99 },
+  ];
+
+  const productosList = document.createElement("ul");
+  document.querySelector(".card-body").appendChild(productosList);
+
+  productosArray.forEach((producto) => {
+    const productoItem = document.createElement("li");
+    productoItem.textContent = `${producto.nombre}: $${producto.precio}`;
+    productosList.appendChild(productoItem);
+  });
 }
 
-let stockDisponible = true;
-if (stockDisponible) {
-  alert("El producto está disponible en stock.");
-} else {
-  alert("El producto no está disponible en stock.");
-}
+// Llamar a la función para cargar la información
+terceraPreentrega();
 
-// Array de productos
-const productosArray = [
-  { nombre: "Producto A", precio: 8.99 },
-  { nombre: "Producto B", precio: 12.49 },
-  { nombre: "Producto C", precio: 15.99 },
-];
 
-// Mostrar información de productos en el DOM
-const productosList = document.createElement("ul");
-document.querySelector(".card-body").appendChild(productosList);
-
-productosArray.forEach((producto) => {
-  const productoItem = document.createElement("li");
-  productoItem.textContent = `${producto.nombre}: $${producto.precio}`;
-  productosList.appendChild(productoItem);
-});
