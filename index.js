149899e5b9aab14ce386def9343f4b40c15ad570
gastoncc <gastonasura99@gmail.com>
2023-09-11 22:03:21 -0300

    entrega final

--- index.js
+++ index.js
@@ -1,106 +1,89 @@
-function saludar() {
-  alert("Primera Preentrega");
-}
-
-function sumar(a, b) {
-  return a + b;
-}
-let resultado = sumar(3, 5);
-console.log(resultado); 
-
-let nombre = prompt("Ingrese su nombre:");
-let apellido = prompt("Ingrese su apellido:");
-alert("Holaaaa! " + nombre + " " + apellido);
-
-
-let nombr = prompt("Ingrese su nombre:");
-let edad = prompt("Ingrese su edad:");
-alert(nombr + " tiene " + edad + " años.");
-
-
-let resultados = 233 + 356 + 478;
-alert("El resultado es: " + resultado);
-
-
-
-const contraseñaCorrecta = "12345";
-const correoCorrecto = "56789";
-let contraseña = prompt("Ingrese su contraseña:");
-let correo = prompt("Ingrese su correo electrónico:");
-while (contraseña !== contraseñaCorrecta || correo !== correoCorrecto) {
-  alert("Contraseña o correo electrónico incorrectos. Inténtelo de nuevo.");
+function entregaFinal() {  
+    // Definir un objeto "producto" 
+    const producto = {  
+      nombre: "ESMALTADO SEMIPERMANENTE",  
+      descripcion: "El semipermanente es un sistema de duración corta...",  
+      precio: 10.99,  
+    };  
   
-  contraseña = prompt("Ingrese su contraseña:");
-  correo = prompt("Ingrese su correo electrónico:");
+    // Almacenar el objeto "producto" en localStorage 
+    localStorage.setItem("producto", JSON.stringify(producto));  
+  
+    // Crear un párrafo para mostrar el precio del producto 
+    const precioParrafo = document.createElement("p");  
+    precioParrafo.textContent = `Precio: $${producto.precio}`;  
+    document.querySelector(".card-body").appendChild(precioParrafo);  
+  
+    // Agregar un evento de clic al botón "pedidoBtn" 
+    document.addEventListener('DOMContentLoaded', function () {
+        const pedidoBtn = document.getElementById('pedidoBtn');
+        pedidoBtn.addEventListener('click', function () {
+            alert('Pedido realizado');
+        });
+    });
+    
+  
+    // Cambiar el color del párrafo de precio según el precio del producto 
+    if (producto.precio > 15) {  
+      precioParrafo.style.color = "red";  
+    } else {  
+      precioParrafo.style.color = "green";  
+    }  
+  
+    // Simular la disponibilidad del producto en stock 
+    let stockDisponible = true;  
+    if (stockDisponible) {  
+      alert("El producto está disponible en stock.");  
+    } else {  
+      alert("El producto no está disponible en stock.");  
+    }  
+  
+    // Definir un array de productos y mostrarlos en una lista 
+    const productosArray = [  
+      { nombre: "Producto A", precio: 8.99 },  
+      { nombre: "Producto B", precio: 12.49 },  
+      { nombre: "Producto C", precio: 15.99 },  
+    ];  
+  
+    const productosList = document.createElement("ul");  
+    document.querySelector(".card-body").appendChild(productosList);  
+  
+    productosArray.forEach((producto) => {  
+      const productoItem = document.createElement("li");  
+      productoItem.textContent = `${producto.nombre}: $${producto.precio}`;  
+      productosList.appendChild(productoItem);  
+    });  
+  
+    // función flecha y destructuracion 
+    const suma = (a, b) => a + b; 
+    const { nombre, descripcion } = producto; 
+  
+    // Librería 
+    $("#pedidoBtn").on("click", () => { 
+      alert("¡Pedido realizado con jQuery!"); 
+    }); 
+  
+    // Promesas con Fetch: Cargar datos desde archivo JSON local 
+    fetch('./datos.json')
+    .then(response => response.json())
+    .then(data => {
+        // Actualiza los elementos HTML con los datos del archivo JSON
+        const cardTitle = document.querySelector('.card-title');
+        const cardText = document.querySelector('.card-text');
+        const cardImage = document.querySelector('.card-img-top');
+
+        cardTitle.textContent = data.titulo;
+        cardText.textContent = data.descripcion;
+        cardImage.src = data.imagen;
+        cardImage.alt = "esmaltado semipermanente"; 
+    })
+    .then(error => {
+        console.error('Error al cargar datos:', error);
+    });
+
 }
-alert("Inicio de sesión exitoso. Bienvenido " + correo + "!");
+ 
+  
+  entregaFinal();
 
 
-let dia = prompt("Ingrese un día de la semana")
-switch (dia) {
-  case "lunes":
-    alert("Hoy es lunes");
-    break;
-  case "martes":
-    alert("Hoy es martes");
-    break;
-  case "miércoles":
-  case "miercoles": 
-    alert("Hoy es miércoles");
-    break;
-  case "jueves":
-    alert("Hoy es jueves");
-    break;
-  case "viernes":
-    alert("Hoy es viernes");
-    break;
-  case "sábado":
-  case "sabado":
-    alert("Hoy es sábado");
-    break;
-  case "domingo":
-    alert("Hoy es domingo");
-    break;
-  default:
-    alert("No se reconoce ese día de la semana");
-}
-
-
-let numeros = [10, 25, 30, 45, 50];
-let numerosMayores30 = numeros.filter(function(numero) {
-  return numero > 30;
-});
-console.log(numerosMayores30);
-
-let numeroBuscado = numeros.find(function(numero) {
-  return numero === 25;
-});
-console.log(numeroBuscado);
-
-let incluyeNumero30 = numeros.includes(30);
-console.log(incluyeNumero30);
-
-let indiceNumero45 = numeros.indexOf(45);
-console.log(indiceNumero45);
-
-
-let persona = [];
-
-while (true) {
-  let nombre = prompt("Ingrese el nombre o 'termine' para terminar):");
-  if (nombre.toLowerCase() === "termine") {
-    break;
-  }
-  let correo = prompt("Ingrese el correo electrónico:");
-  let dni = prompt("Ingrese el DNI:");
-  let fechaNacimiento = prompt("Ingrese la fecha de nacimiento (dd/mm/aaaa):");
-  let persona = {
-    nombre: nombre,
-    correo: correo,
-    dni: dni,
-    fechaNacimiento: fechaNacimiento
-  };
-  personas.push(persona);
-}
-console.log("Datos ingresados:");
-console.log(persona);
